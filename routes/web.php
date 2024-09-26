2fc5fb76d32ae63c5bb16fd4497a3fbf499c64dd
abhatt014 <amitfreeze@gmail.com>
2024-09-26 10:58:07 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view('/test', 'test');
+
+Route::get('/about/{name}',function($name) {    
+    return view('about',['name'=>$name]);   
+ });
